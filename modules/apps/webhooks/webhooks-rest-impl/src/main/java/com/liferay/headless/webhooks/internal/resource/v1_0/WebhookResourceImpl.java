df00d60062d47c608bde1866cb082b6903b6574b
Luismi Barcos <luismiguel.barcos@liferay.com>
2021-11-17 21:37:21 +0100

    LPS-139667 Add a basic rest implementation

--- modules/apps/webhooks/webhooks-rest-impl/src/main/java/com/liferay/headless/webhooks/internal/resource/v1_0/WebhookResourceImpl.java
+++ modules/apps/webhooks/webhooks-rest-impl/src/main/java/com/liferay/headless/webhooks/internal/resource/v1_0/WebhookResourceImpl.java
@@ -14,10 +14,20 @@
 
 package com.liferay.headless.webhooks.internal.resource.v1_0;
 
+import com.liferay.headless.webhooks.dto.v1_0.Webhook;
+import com.liferay.headless.webhooks.internal.dto.v1_0.converter.WebhookDTOConverter;
 import com.liferay.headless.webhooks.resource.v1_0.WebhookResource;
-
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
+import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.webhooks.service.WebhookService;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
+
+import java.util.Collections;
 
 /**
  * @author Luis Miguel Barcos
@@ -27,4 +37,52 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = WebhookResource.class
 )
 public class WebhookResourceImpl extends BaseWebhookResourceImpl {
+
+	@Override
+	public Webhook getWebhook(String webhookId) throws Exception {
+		return _toWebhook(
+			_webhookService.getWebhook(GetterUtil.getLong(webhookId)));
+	}
+
+	@Override
+	public Page<Webhook> getSiteWebhooksPage(
+		Long siteId) throws Exception {
+		return Page.of(
+			transform(
+				_webhookService.getSiteWebhooks(siteId),
+				this::_toWebhook
+			)
+		);
+	}
+
+	@Override
+	public Webhook postSiteWebhook(
+		Long siteId, Webhook webhook) throws Exception {
+		ServiceContext serviceContext = new ServiceContext();
+		serviceContext.setScopeGroupId(siteId);
+		return _toWebhook(_webhookService.addWebhook(webhook.getApiKey(),
+			contextUser.getUserId(), webhook.getUrl(), serviceContext));
+	}
+
+	@Override
+	public void deleteWebhook(String webhookId) throws Exception {
+		_webhookService.deleteWebhook(GetterUtil.getLong(webhookId));
+	}
+
+	private Webhook _toWebhook(com.liferay.webhooks.model.Webhook webhook) {
+		return _webhookDTOConverter.toDTO(new DefaultDTOConverterContext(
+			contextAcceptLanguage.isAcceptAllLanguages(),
+			Collections.emptyMap(), _dtoConverterRegistry,
+			webhook.getWebhookId(), contextAcceptLanguage.getPreferredLocale(),
+			contextUriInfo, contextUser), webhook);
+	}
+
+	@Reference
+	private WebhookService _webhookService;
+
+	@Reference
+	private WebhookDTOConverter _webhookDTOConverter;
+
+	@Reference
+	private DTOConverterRegistry _dtoConverterRegistry;
 }
