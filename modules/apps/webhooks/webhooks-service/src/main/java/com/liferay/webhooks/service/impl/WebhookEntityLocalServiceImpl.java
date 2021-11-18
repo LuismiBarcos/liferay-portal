52f48f9c2182457dbcf42182ca7695458d2cdff4
Luismi Barcos <luismiguel.barcos@liferay.com>
2021-11-18 18:39:07 +0100

    LPS-139667 Add WebhookEntity methods implementation

--- modules/apps/webhooks/webhooks-service/src/main/java/com/liferay/webhooks/service/impl/WebhookEntityLocalServiceImpl.java
+++ modules/apps/webhooks/webhooks-service/src/main/java/com/liferay/webhooks/service/impl/WebhookEntityLocalServiceImpl.java
@@ -15,9 +15,14 @@
 package com.liferay.webhooks.service.impl;
 
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.webhooks.model.WebhookEntity;
 import com.liferay.webhooks.service.base.WebhookEntityLocalServiceBaseImpl;
-
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
@@ -28,4 +33,28 @@ import org.osgi.service.component.annotations.Component;
 )
 public class WebhookEntityLocalServiceImpl
 	extends WebhookEntityLocalServiceBaseImpl {
+
+	@Override
+	public WebhookEntity addWebhookEntity(
+		String entityClassName, long userId, long webhookId,
+		ServiceContext serviceContext) throws PortalException {
+
+		User user = _userLocalService.getUser(userId);
+		long groupId = serviceContext.getScopeGroupId();
+		long webhookEntityId = counterLocalService.increment();
+
+		WebhookEntity webhookEntity = webhookEntityPersistence.create(webhookEntityId);
+		webhookEntity.setUuid(serviceContext.getUuid());
+		webhookEntity.setGroupId(groupId);
+		webhookEntity.setCompanyId(user.getCompanyId());
+		webhookEntity.setUserId(user.getUserId());
+		webhookEntity.setUserName(user.getFullName());
+		webhookEntity.setEntityClassName(entityClassName);
+		webhookEntity.setWebhookId(webhookId);
+
+		return webhookEntityPersistence.update(webhookEntity);
+	}
+
+	@Reference
+	private UserLocalService _userLocalService;
 }
--- modules/apps/webhooks/webhooks-service/src/main/java/com/liferay/webhooks/service/impl/WebhookEntityServiceImpl.java
+++ modules/apps/webhooks/webhooks-service/src/main/java/com/liferay/webhooks/service/impl/WebhookEntityServiceImpl.java
@@ -15,9 +15,15 @@
 package com.liferay.webhooks.service.impl;
 
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.webhooks.model.WebhookEntity;
+import com.liferay.webhooks.service.WebhookEntityLocalService;
 import com.liferay.webhooks.service.base.WebhookEntityServiceBaseImpl;
-
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import java.util.List;
 
 /**
  * @author Brian Wing Shun Chan
@@ -30,4 +36,26 @@ import org.osgi.service.component.annotations.Component;
 	service = AopService.class
 )
 public class WebhookEntityServiceImpl extends WebhookEntityServiceBaseImpl {
+
+	@Override
+	public WebhookEntity deleteWebhookEntity(long webhookEntityId)
+		throws PortalException {
+		return _webhookEntityLocalService.deleteWebhookEntity(webhookEntityId);
+	}
+
+	@Override
+	public List<WebhookEntity> getSiteWebhookEntities(long groupId) {
+		return webhookEntityPersistence.findByGroupId(groupId);
+	}
+
+	@Override
+	public WebhookEntity addWebhookEntity(
+		String entityClassName, long userId, long webhookId,
+		ServiceContext serviceContext) throws PortalException {
+		return _webhookEntityLocalService.addWebhookEntity(
+			entityClassName, userId, webhookId, serviceContext);
+	}
+	
+	@Reference
+	private WebhookEntityLocalService _webhookEntityLocalService;
 }
