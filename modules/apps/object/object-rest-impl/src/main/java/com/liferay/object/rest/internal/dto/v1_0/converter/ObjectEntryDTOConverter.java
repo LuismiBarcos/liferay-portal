dcd46fbba867ca6f314606da886c46fd6d824178
Luismi Barcos <luismiguel.barcos@liferay.com>
2022-03-28 21:51:02 +0200

    LPS-XXXX Admit return M:N relationships through APIs

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/dto/v1_0/converter/ObjectEntryDTOConverter.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/dto/v1_0/converter/ObjectEntryDTOConverter.java
@@ -34,6 +34,7 @@ import com.liferay.object.service.ObjectFieldLocalService;
 import com.liferay.object.service.ObjectRelationshipLocalService;
 import com.liferay.object.util.ObjectEntryFieldValueUtil;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.service.GroupLocalService;
@@ -56,6 +57,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.UriInfo;
@@ -314,6 +316,59 @@ public class ObjectEntryDTOConverter
 			else {
 				map.put(objectFieldName, serializable);
 			}
+		}
+
+		if(nestedFieldsDepth > 0) {
+			List<ObjectRelationship> objectRelationships =
+				_objectRelationshipLocalService.getObjectRelationships(
+					objectDefinition.getObjectDefinitionId());
+
+			List<ObjectRelationship> manyToManyRelationships =
+				objectRelationships.stream().filter(
+					objectRelationship -> Objects.equals(
+						objectRelationship.getType(),
+						ObjectRelationshipConstants.TYPE_MANY_TO_MANY)).collect(
+					Collectors.toList());
+
+			manyToManyRelationships.forEach(objectRelationship -> {
+				try {
+
+					boolean reverse = objectRelationship.isReverse();
+
+					if (objectRelationship.isReverse()) {
+						objectRelationship =
+							_objectRelationshipLocalService.fetchReverseObjectRelationship(
+								objectRelationship, false);
+					}
+
+					List<com.liferay.object.model.ObjectEntry>
+						manyToManyRelatedObjectEntries =
+						_objectEntryLocalService.getManyToManyRelatedObjectEntries(
+							objectEntry.getGroupId(),
+							objectRelationship.getObjectRelationshipId(),
+							objectEntry.getObjectEntryId(), reverse, 0, 20);
+
+					Object[] objectEntries =
+						manyToManyRelatedObjectEntries.stream().map(
+							objectEntry1 -> {
+								try {
+									return _toDTO(
+										_getDTOConverterContext(
+											dtoConverterContext,
+											objectEntry1.getObjectEntryId()),
+										nestedFieldsDepth - 1, objectEntry1);
+								}
+								catch (Exception e) {
+									e.printStackTrace();
+									return null;
+								}
+							}).toArray();
+					map.put(objectRelationship.getName(), objectEntries);
+				}
+				catch (PortalException e) {
+					e.printStackTrace();
+				}
+			});
 		}
 
 		values.remove(objectDefinition.getPKObjectFieldName());
