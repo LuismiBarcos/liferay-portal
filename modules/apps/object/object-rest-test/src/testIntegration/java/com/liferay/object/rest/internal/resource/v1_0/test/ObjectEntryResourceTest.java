84d9bd9350d22fbf6c25759f60ffbee471b04109
Luismi Barcos <luismiguel.barcos@liferay.com>
2022-12-18 19:36:58 +0100

    LPS-170415 Add test to get object entries filtered by related object entries field values

--- modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
+++ modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
@@ -206,6 +206,66 @@ public class ObjectEntryResourceTest {
 	}
 
 	@Test
+	public void testGetFilteredObjectEntriesByRelatedObjectEntries()
+		throws Exception {
+
+		PropsUtil.addProperties(
+			UnicodePropertiesBuilder.setProperty(
+				"feature.flag.LPS-154672", "true"
+			).build());
+
+		_objectRelationship = _addObjectRelationshipAndRelateObjectsEntries(
+			ObjectRelationshipConstants.TYPE_MANY_TO_MANY);
+
+		_testGetFilteredObjectEntriesByRelatedObjectEntries(
+			StringBundler.concat(
+				_objectDefinition1.getRESTContextPath(), "?filter=",
+				_objectRelationship.getName(), StringPool.SLASH,
+				_OBJECT_FIELD_NAME_2, "%20eq%20",
+				String.valueOf(_OBJECT_FIELD_VALUE_2)));
+
+		_testGetFilteredObjectEntriesByRelatedObjectEntries(
+			StringBundler.concat(
+				_objectDefinition1.getRESTContextPath(), "?filter=",
+				_objectRelationship.getName(), StringPool.SLASH,
+				_OBJECT_FIELD_NAME_2, "%20gt%20",
+				String.valueOf(_OBJECT_FIELD_VALUE_2 - 1)));
+
+		_testGetFilteredObjectEntriesByRelatedObjectEntries(
+			StringBundler.concat(
+				_objectDefinition1.getRESTContextPath(), "?filter=",
+				_objectRelationship.getName(), StringPool.SLASH,
+				_OBJECT_FIELD_NAME_2, "%20ge%20",
+				String.valueOf(_OBJECT_FIELD_VALUE_2 - 1)));
+
+		_testGetFilteredObjectEntriesByRelatedObjectEntries(
+			StringBundler.concat(
+				_objectDefinition1.getRESTContextPath(), "?filter=",
+				_objectRelationship.getName(), StringPool.SLASH,
+				_OBJECT_FIELD_NAME_2, "%20le%20",
+				String.valueOf(_OBJECT_FIELD_VALUE_2 + 1)));
+
+		_testGetFilteredObjectEntriesByRelatedObjectEntries(
+			StringBundler.concat(
+				_objectDefinition1.getRESTContextPath(), "?filter=",
+				_objectRelationship.getName(), StringPool.SLASH,
+				_OBJECT_FIELD_NAME_2, "%20lt%20",
+				String.valueOf(_OBJECT_FIELD_VALUE_2 + 1)));
+
+		_testGetFilteredObjectEntriesByRelatedObjectEntries(
+			StringBundler.concat(
+				_objectDefinition1.getRESTContextPath(), "?filter=",
+				_objectRelationship.getName(), StringPool.SLASH,
+				_OBJECT_FIELD_NAME_2, "%20ne%20",
+				String.valueOf(_OBJECT_FIELD_VALUE_2 - 1)));
+
+		PropsUtil.addProperties(
+			UnicodePropertiesBuilder.setProperty(
+				"feature.flag.LPS-154672", "false"
+			).build());
+	}
+
+	@Test
 	public void testGetNestedFieldDetailsInOneToManyRelationships()
 		throws Exception {
 
@@ -382,6 +442,23 @@ public class ObjectEntryResourceTest {
 		JSONArray itemsJSONArray = jsonObject.getJSONArray("items");
 
 		Assert.assertEquals(1, itemsJSONArray.length());
+	}
+
+	private void _testGetFilteredObjectEntriesByRelatedObjectEntries(
+			String endpoint)
+		throws Exception {
+
+		JSONObject jsonObject = HTTPTestUtil.invoke(
+			null, endpoint, Http.Method.GET);
+
+		JSONArray itemsJSONArray = jsonObject.getJSONArray("items");
+
+		Assert.assertEquals(1, itemsJSONArray.length());
+
+		JSONObject itemJSONObject = itemsJSONArray.getJSONObject(0);
+
+		Assert.assertEquals(
+			_OBJECT_FIELD_VALUE_1, itemJSONObject.getInt(_OBJECT_FIELD_NAME_1));
 	}
 
 	private void _testGetNestedFieldDetailsInOneToManyRelationships(
