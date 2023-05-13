7f62bcc1305ade4ffe7ba27a364fadf9fc2060c9
Luismi Barcos <luismiguel.barcos@liferay.com>
2023-05-13 21:29:52 +0200

    LPS-181554 Add Sourcer abstraction as an entry to the Liferay API Builder generator. The implementation of this abstraction should provide the needed information to build the REST applications.

---
+++ modules/apps/headless/headless-builder/headless-builder-impl/src/main/java/com/liferay/headless/builder/internal/sourcer/api/Sourcer.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.builder.internal.sourcer.api;
+
+import com.liferay.portal.vulcan.yaml.openapi.Schema;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import javax.ws.rs.core.UriInfo;
+
+/**
+ * @author Luis Miguel Barcos
+ */
+public interface Sourcer {
+
+	public Map<String, PropertyInfo> getPropertiesInfo(
+		String entityName, Map<String, Schema> propertySchemas);
+
+	public Object getValue(
+			PropertyInfo propertyInfo, Object pathParameterValue,
+			HttpServletRequest httpServletRequest, UriInfo uriInfo)
+		throws Exception;
+
+}
